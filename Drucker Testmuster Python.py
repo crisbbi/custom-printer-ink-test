0c533b28593da463ace1e619b5bb9f77a90ae0cd
Christian Biasi <christianbiasi@gmx.de>
2019-09-09 00:02:21 +0200

    Tesseract starts to work
    Set up tesseract, pytesseract, PATH variables with v3.05. After a restart tesseract works.

--- Drucker Testmuster Python.py
+++ Drucker Testmuster Python.py
@@ -1,6 +1,8 @@
 import pyautogui
 import pytesseract
+import re
 
 #pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files (x86)/Tesseract-OCR/tesseract'
 screenshot = pyautogui.screenshot('test.png')
-print(pytesseract.image_to_data(screenshot))
+extractedText = pytesseract.image_to_string(screenshot)
+print(extractedText)
