e8116277efb81d800ddea83eda4a4acdd752b2fb
Christian Biasi <christianbiasi@gmx.de>
2019-09-09 01:28:21 +0200

    OCR data now as list

--- Drucker Testmuster Python.py
+++ Drucker Testmuster Python.py
@@ -4,8 +4,12 @@ import re
 
 #pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files (x86)/Tesseract-OCR/tesseract'
 screenshot = pyautogui.screenshot('test.png')
-extractedText = pytesseract.image_to_string(screenshot)
-print(extractedText)
+extractedText = pytesseract.image_to_data(screenshot)
+#print(extractedText)
+extractedText = extractedText.replace("\t"," ")
+extractedText = extractedText.replace("\n"," ")
+extractedArray = extractedText.split(" ")
+print(extractedArray)
 
 if re.search("D.*test", extractedText):
-    print("Match: " + str(re.search("D.*test", extractedText)))
+    print("Match: " + str(re.search("D[^0-9]*test", extractedText)))
